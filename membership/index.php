018e70ac64b485e97663bd27c7d71d65a1b9b4aa
Aklilegetachew <aklilegetachew@gmail.com>
2023-02-13 14:13:27 +0300

    membership fixed

---
+++ membership/index.php
@@ -0,0 +1,49 @@
+<?php
+
+
+if ($user_id !== 5102867263 && $stepMem == "MembershipStart") {
+    if ($text == "Next") {
+        sharePhone($chat_id);
+        setStep($user_id, "MemberFinal");
+    } elseif ($text == "Go Back") {
+        BackNotif($update);
+        // goto QUAN;
+    } elseif ($text == "Cancel") {
+
+        $userIdDb = getUserID($user_id);
+        $UserInfo = getUserInput($userIdDb);
+        CancelNotifyerUser($chat_id);
+        DeletRequest($userIdDb);
+    } elseif ($text !== "Cancel" && $text !== "Go Back" && $text !== "Next" && $text !== "submit") {
+        $markup  = array('keyboard' => array(array('Next'), array('Go Back', 'Cancel')), 'resize_keyboard' => true, 'selective' => true, 'one_time_keyboard' => true);
+        $markupjs = json_encode($markup);
+        $msg = urlencode("please choose only from the options provided. or cancel the previous order to empty your cart\nካሉት አማራጮች ብቻ ይምረጡ");
+
+        message($chat_id, $msg, $markupjs);
+    }
+}
+
+// =============================== Membership setting phone number ================================
+
+if ($Contact && $stepMem == "MemberFinal") {
+    setphone($user_id, $Contact);
+    ChooseProvider($chat_id, $user_id);
+    setStep($user_id, "MembershipCheckout");
+} elseif ($text == "Back" && $stepMem == "MemberFinal") {
+    $userIdDb = getUserIDMem($user_id);
+    BackNotif($update);
+    setStep($user_id, "MembershipStart");
+} elseif ($step == "MemberFinal" && !is_numeric($text) && $text !== "Cancel") {
+    $markup  = array('keyboard' => array(array(array('text' => 'Cancel'), array('text' => 'Phone Number', 'request_contact' => true))), 'resize_keyboard' => true, 'one_time_keyboard' => true, 'selective' => true);
+    $markupjs = json_encode($markup);
+    message($chat_id, "Please insert Phone number! or cancel previous order", $markupjs);
+} elseif (is_numeric($text) && $stepMem == "MemberFinal") {
+    setphone($user_id, $text);
+    ChooseProviderMem($chat_id, $user_id);
+    setStep($user_id, "MembershipCheckout");
+} elseif ($text == "Cancel"  && $stepMem == "MemberFinal") {
+    $userIdDb = getUserID($user_id);
+    $UserInfo = getUserInput($userIdDb);
+    CancelNotifyerUser($chat_id);
+    DeletRequest($userIdDb);
+}
